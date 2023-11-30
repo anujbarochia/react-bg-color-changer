d1202cd67f9a1f3a94efe3ffc060d4d1daae1ddb
anujbarochia <anujbarochia24@gmail.com>
2023-11-30 17:44:57 +0530

    project with my initial commented code,where i wasn't able to pass the setbgcolor properly and there is also fixed code with uncomment

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import DisplayColorBlocks from "./DisplayColorBlocks";
+
+// function App() {
+//   let [bgColor, setbgColor] = useState("Orange");
+
+//   <div className="text-center" style={{ backgroundColor: bgColor }}>
+//     <br />
+//     <div className="  py-10 mx-20 border-4 border-double rounded-3xl border-orange-950">
+//       <h1 className="text-bold text-2xl"> This is Background Changer</h1>
+//       <br />
+//       <p className="bg-slate-400 h-8">
+//         Click Below to change the{" "}
+//         <span className="text-xl ">Background colour</span>
+//       </p>
+//     </div>
+//   </div>;
+
+//   {
+//     DisplayColorBlocks({ setbgColor });
+//   }
+// }
+
+// export default App;
+
+function App() {
+  const [bgColor, setbgColor] = useState("Orange");
+
+  return (
+    <div
+      className="text-center h-screen w-screen"
+      style={{ backgroundColor: bgColor }}
+    >
+      <br />
+      <div className="py-10 mx-20 border-4 border-double rounded-3xl border-orange-950 my-10">
+        <h1 className="text-bold text-2xl">This is Background Changer</h1>
+        <br />
+        <p className="bg-slate-400 h-8">
+          Click Below to change the{" "}
+          <span className="text-xl">Background colour</span>
+        </p>
+      </div>
+
+      <DisplayColorBlocks setbgColor={setbgColor} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DisplayColorBlocks.js
@@ -0,0 +1,68 @@
+// import React from "react";
+// const ColorObj = [
+//   { tailwindClass: "bg-green-500", hex: "#22c55e", colorName: "Green" },
+//   { tailwindClass: "bg-rose-800", hex: "#be123c", colorName: "Rose" },
+//   { tailwindClass: "bg-blue-500", hex: "#3b82f6", colorName: "Blue" },
+//   { tailwindClass: "bg-slate-500", hex: "#64748b", colorName: "Grey" },
+//   { tailwindClass: "bg-cyan-500", hex: "#06b6d4", colorName: "Cyan" },
+//   { tailwindClass: "bg-pink-400", hex: "#f472b6", colorName: "Pink" },
+//   { tailwindClass: "bg-violet-400", hex: "#a78bfa", colorName: "Violet" },
+//   { tailwindClass: "bg-teal-500", hex: "#14b8a6", colorName: "Teal" },
+//   { tailwindClass: "bg-lime-500", hex: "#365314", colorName: "Olive" },
+//   { tailwindClass: "bg-emerald-400", hex: "#34d399", colorName: "Emerald" },
+//   { tailwindClass: "bg-sky-300", hex: "#7dd3fc", colorName: "Sky" },
+// ];
+
+// function DisplayColorBlocks(setbgColor) {
+//   function handle(toSetColor) {
+//     setbgColor(toSetColor);
+//   }
+
+//   ColorObj.map((color) => {
+//     return (
+//       <button className={color.tailwindClass} onClick={() => handle(color.hex)}>
+//         {color.colorName}
+//       </button>
+//     );
+//   });
+// }
+
+// export default DisplayColorBlocks;
+
+import React from "react";
+
+const ColorObj = [
+  { tailwindClass: "bg-green-500", hex: "#22c55e", colorName: "Green" },
+  { tailwindClass: "bg-rose-800", hex: "#be123c", colorName: "Rose" },
+  { tailwindClass: "bg-blue-500", hex: "#3b82f6", colorName: "Blue" },
+  { tailwindClass: "bg-slate-500", hex: "#64748b", colorName: "Grey" },
+  { tailwindClass: "bg-cyan-500", hex: "#06b6d4", colorName: "Cyan" },
+  { tailwindClass: "bg-pink-400", hex: "#f472b6", colorName: "Pink" },
+  { tailwindClass: "bg-violet-400", hex: "#a78bfa", colorName: "Violet" },
+  { tailwindClass: "bg-teal-500", hex: "#14b8a6", colorName: "Teal" },
+  { tailwindClass: "bg-lime-500", hex: "#365314", colorName: "Olive" },
+  { tailwindClass: "bg-emerald-400", hex: "#34d399", colorName: "Emerald" },
+  { tailwindClass: "bg-sky-300", hex: "#7dd3fc", colorName: "Sky" },
+];
+
+function DisplayColorBlocks({ setbgColor }) {
+  function handle(toSetColor) {
+    setbgColor(toSetColor);
+  }
+
+  return (
+    <div>
+      {ColorObj.map((color) => (
+        <button
+          key={color.colorName}
+          className={`${color.tailwindClass} p-10 m-10 border-2 rounded-lg w-30`}
+          onClick={() => handle(color.hex)}
+        >
+          {color.colorName}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default DisplayColorBlocks;
